5138c1c5a16114828e68a6b38a3ef11b3a4bdcc0
Binazir <binazir.farokhi@gmail.com>
2023-04-24 12:42:43 -0700

    showapi

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// const btn = document.getElementById('btn');
-// const showImage = document.getElementById('showImage');
+const btn = document.getElementById('btn');
+const showImage = document.getElementById('showImage');
 
 // async function fetchData() {
 //     try {
@@ -13,33 +13,50 @@
 //   }
   
 
-// btn.addEventListener('click',function(){
-//     alet
-//     showImage.innerHTML = `  <img src="${  data.message}" alt="" id="image">`
-//     fetchData();
+// btn.addEventListener('click',function(fetchData()){
+    
+//     showImage.innerHTML = `  <img src="${data.message}" alt="" id="image">`
+    
 // })
 
-const btn = document.getElementById('btn');
-const showImage = document.getElementById('showImage');
+// const btn = document.getElementById('btn');
+// const showImage = document.getElementById('showImage');
 
-btn.addEventListener('click', async function(){
-    try {
-        const data = await fetchData();
-        showImage.innerHTML = `<img src="${data.message}" alt="">`;
-    } catch (error) {
-        console.log(error);
-    }
+// btn.addEventListener('click', async function(){
+//     try {
+//         const data = await fetchData();
+//         showImage.innerHTML = `<img src="${data.message}" alt="">`;
+//     } catch (error) {
+//         console.log(error);
+//     }
+// })
+
+// async function fetchData() {
+//     try {
+//       const response = await fetch('https://dog.ceo/api/breeds/image/random');
+//       const data = await response.json();
+//       console.log(data);
+//       return data;
+//     } catch (error) {
+//       console.log(error);
+//     }
+// }
+
+btn.addEventListener('click',()=> {
+
+    getApi()
 })
 
-async function fetchData() {
-    try {
-      const response = await fetch('https://dog.ceo/api/breeds/image/random');
-      const data = await response.json();
-      console.log(data);
-      return data;
-    } catch (error) {
-      console.log(error);
+async function getApi(){
+    try{
+          const response = await fetch ('https://dog.ceo/api/breeds/image/random');
+    const data =  await response.json();
+    console.log(data);
+    showImage.innerHTML = `<img src="${data.message}" alt="">`;
+    return data;
     }
-}
+  catch(error){
+console.log(error);
+  }
 
-  
+}
